a1dc04478a9dde99d5bd28ec601c967a02192932
Laurent Kadeko <laka020402@skovde.se>
2024-02-09 16:12:46 +0100

    checked
    Forgot to add check function

--- script.js
+++ script.js
@@ -23,7 +23,11 @@ function addTask(){
 }
 
 listContainer.addEventListener("click", function(e){
-  if(e.target.tagName === "SPAN"){
+  if(e.target.tagName === "LI"){
+    e.target.classList.toggle("checked")
+  }
+
+  else if(e.target.tagName === "SPAN") {
     e.target.parentElement.remove();
   }
 },false);
