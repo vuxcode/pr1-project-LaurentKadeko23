6622cecc4966c30ae3581c7ab34fc68324df8547
LaurentKadeko23 <laurentmugishakadeko@gmail.com>
2024-02-22 23:35:15 +0100

    creating renderTodos

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const { render } = require("react-dom");
 
 
 const todoForm = document.querySelectorAll(".todo-form")
@@ -5,8 +6,33 @@ const todoInput= document.querySelectorAll(".todo-input")
 const todoItemList= document.querySelectorAll(".todo-items")
 
 // add todo
+let todos = [];
 todoForm.addEventListener("submit", function(event) {
   // prevent the page from reloading
   event.preventDefault();
   addTodo(todoInput.value) // call addTodo function with input box
-})
+});
+
+function addTodo(item) {
+  // if item is not empty
+  if(item !== " ") {
+    // make a todo object, which has an ide, name, and completed properties 
+    const todo = {
+      it: Date.now(),
+      name: item,
+      completed: false
+    };
+
+    // tadd it to todos array
+    todos.push(todo);
+    renderTodos(todos)
+
+    // clearing the input box value
+    todoInput = " ";
+  }
+}
+
+// function to render given todos to screen
+function renderTodos(tods) {
+  // clear everything inside ul
+}
