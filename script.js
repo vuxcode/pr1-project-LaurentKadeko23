17ec47a798999eed4c650ec1efa9aa8091cd9536
LaurentKadeko23 <laurentmugishakadeko@gmail.com>
2024-02-23 14:15:33 +0100

    adding click listener to checkbox

--- script.js
+++ script.js
@@ -17,51 +17,31 @@ todos.forEach(function(todo){
   const img = document.createElement("img")
 
   // adding classes
-  
+  card.classList.add("card");
+  button.classList.add("clear");
+  cbContainer.classList.add("cb-container");
+  cbInput.classList.add("cb-input");
+  item.classList.add("item");
+  check.classList.add("check");
+  button.classList.add("clear")
 
+  // set atrributes 
+  card.setAttribute("draggable", true);
+  img.setAttribute("src", "./assets/images/icon-cross.svg")
+  img.setAttribute("alt", "Clear it")
+  cbContainer.setAttribute("type", "checkbox");
+
+  // set todo item for card
+  item.textContent = todo.item;
+
+  // if completed -> add respective class / attributes
+
+  if(todo.isCompleted){
+    card.classList.add("checked");
+    cbContainer.setAttribute("checed", "checked")
+  }
+
+  // add click listener to chechbox 
 })
 
 
-// drag over on .todos container
-document.querySelector(".todos").addEventListener("dragover"), function(e) {
-  e.preventDefault();
-  if(!e.target.classList.contains("dragging") &&
-  !e.target.classList.contains("card") 
-  ) {
-    const draggingCard = document.querySelectorAll("card");
-  }
-
-  const currPos = cards.index.Of(draggingCard);
-  const newPos = cards.indexOf(e.target);
-  console.log(currPos, newPos)
-  if(currPos > newPos) {
-    this.insertBefore(draggingCard, e.target);
-  }else{
-    this.insertBefore(draggingCard, e.target.nextSibling);
-  }
-  const todos = JSON.parse(localStorage.getItemS("todos"));
-  const removed = todos.splice(currPos, 1);
-  todos.splice(newPos, 0, removed[0]);
-  localStorage.setItem("todos", JSON.stringify(todos))
-}
-
-// addd new todos on user input
-const add = document.getElementById("add-btn");
-const txtInput = document.querySelector(".txt-input");
-add.addEventListener("click", function() {
-  const item = txtInput.value.trim();
-  if(item) {
-    txtInput.value = " ";
-    const todos = !localStorage.getItem("todos")
-    ? []
-    :JSON.parse(localStorage.getItem("todos"));
-    const currentTodo = {
-      item,
-      isCompleted: false,
-    };
-    addTodo([currentTodo]);
-    todos.push(currentTodo);
-    localStorage.setItem("todos", JSON.stringify(todos))
-  }
-  txtInput.focus();
-})
